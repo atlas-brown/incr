c4926e1c27b08e0601af98d1a5fa0fdbb757ff2a
Yizheng Xie <yizhengx@bu.edu>
2025-09-03 20:01:11 -0600

    Update scripts

--- setup.sh
+++ setup.sh
@@ -4,8 +4,10 @@ cd $(dirname $0)
 
 # install Rust if not installed
 if ! command -v cargo &> /dev/null; then
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 fi
+
+. "$HOME/.cargo/env"  
 
 # build tee-cache
 cd tee-cache || exit
