f179441b27de679b815f4c8ff286596b041ad8cf
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-09-13 12:28:37 -0400

    refine command hash

--- python/cache.py
+++ python/cache.py
@@ -1,34 +1,65 @@
 #!/usr/bin/env python3
 
 import base64
+from enum import IntEnum
 import hashlib
 from io import BytesIO
 import json
 from json import JSONDecodeError
+import os
 from pathlib import Path
 import subprocess
 import sys
 from threading import Thread
 from typing import Any, IO, Optional
 
-CHUNK_SIZE: int = 65536
-CACHE_DIRECTORY: Path = Path("outputs")
+class CacheEncoding(IntEnum):
+    """
+    The encoding used to save stdin, stdout, and stderr bytes in cache files. UTF-8 is mainly
+    useful for debugging but may not support all bytes while base64 can represent all bytes.
+    """
+    UTF8 = 0
+    Base64 = 1
 
-def generate_command_hash(args: list[str], input: Optional[bytes]):
-    encoded_input = None
-    if input is not None:
-        encoded_input = base64.b64encode(input).decode("utf-8")
+# Constant parameters
+CACHE_ENCODING: CacheEncoding = CacheEncoding.UTF8
+CACHE_DIRECTORY: Path = Path("outputs")
+STREAM_CHUNK_SIZE: int = 65536
+
+def encode_bytes(data: bytes) -> str:
+    """Encodes raw bytes as a string which is storable in a JSON object."""
+    if CACHE_ENCODING == CacheEncoding.UTF8:
+        return data.decode("utf-8")
+    elif CACHE_ENCODING == CacheEncoding.Base64:
+        return base64.b64encode(data).decode("utf-8")
+    raise RuntimeError("Invalid encoding mode")
+
+def decode_bytes(data: str) -> bytes:
+    """Decodes a data string stored in a JSON object back into bytes."""
+    if CACHE_ENCODING == CacheEncoding.UTF8:
+        return data.encode("utf-8")
+    elif CACHE_ENCODING == CacheEncoding.Base64:
+        return base64.b64decode(data)
+    raise RuntimeError("Invalid encoding mode")
+
+def generate_command_hash(
+    args: list[str], 
+    stdin: Optional[bytes],
+    env: dict[str, str],
+) -> tuple[str, dict[str, Any]]:
+    """Converts the command input into a hash used as the cache key."""
     data = {
         "args": args,
-        "encoded_input": encoded_input,
+        "stdin": encode_bytes(stdin) if stdin is not None else None,
+        "env": env,
     }
     hash = hashlib.sha256()
     hash.update(json.dumps(data, sort_keys=True).encode("utf-8"))
-    return hash.hexdigest()
+    return (hash.hexdigest(), data)
 
 def read_stream(stream: IO[bytes], destination: IO[bytes], record: BytesIO):
     try:
-        for chunk in iter(lambda: stream.read(CHUNK_SIZE), b""):
+        for chunk in iter(lambda: stream.read(STREAM_CHUNK_SIZE), b""):
             if chunk:
                 destination.write(chunk)
                 destination.flush()
@@ -45,7 +76,7 @@ def write_stream(stream: IO[bytes], data: bytes):
         total_length = len(data_view)
         sent_count = 0
         while sent_count < total_length:
-            count = stream.write(data_view[sent_count:sent_count + CHUNK_SIZE])
+            count = stream.write(data_view[sent_count:sent_count + STREAM_CHUNK_SIZE])
             sent_count += count if count is not None else 0
         stream.flush()
     finally:
@@ -104,11 +135,15 @@ def main():
         sys.exit()
 
     args = sys.argv[1:]
-    input = None
+    stdin = None
     if not sys.stdin.isatty():
-        input = sys.stdin.buffer.read()
-    hash = generate_command_hash(args, input)
+        stdin = sys.stdin.buffer.read()
+    hash, key_data = generate_command_hash(args, stdin, dict(os.environ))
     cache_file = CACHE_DIRECTORY / f"{hash}.json"
+
+    print(hash, cache_file)
+    print(key_data)
+    sys.exit()
 
     cache_data = None
     try:
