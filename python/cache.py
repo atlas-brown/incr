65a976d79bea0e8cd25ff03fbaddb2f9a0e31b41
jxia3 <jxia3@node0.jxia3-271145.brown-atlas-pg0.utah.cloudlab.us>
2025-09-22 11:42:14 -0600

    small file

--- python/cache.py
+++ python/cache.py
@@ -196,7 +196,7 @@ def run_command(hash: str, command_directory: Path, args: list[str], stdin: Opti
     try_directory = command_directory / TRY_DIRECTORY
     trace_command = [
         STRACE_COMMAND, "-y", "-f", "--seccomp-bpf", "--trace=fork,clone,%file",
-        "-o", f"/tmp/{trace_file}", "env", "-i", "bash", "-c", " ".join(args),
+        "-o", f"/tmp/{trace_file}", "env", "-i", " ".join(args),
     ]
     try_command = [TRY_COMMAND, "-D", str(try_directory)] + trace_command
 
