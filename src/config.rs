a50ce29d198d2e08156379c89f71840768f7cd49
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-09-30 20:06:52 -0600

    add constants

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,7 @@
 pub const CACHE_DIRECTORY: &str = "cache";
+pub const DATA_FILE: &str = "data.json";
+pub const SANDBOX_DIRECTORY: &str = "sandbox";
+pub const OUTPUT_DIRECTORY: &str = "outputs";
+pub const COMMIT_DIRECTORY: &str = "commit";
 pub const DEBUG_FILE: &str = "debug_info.json";
 pub const DEBUG: bool = true;
