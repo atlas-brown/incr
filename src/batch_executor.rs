1b00e80b930613fa02e681e8c2f3b053b8768f05
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-09-30 19:26:34 -0600

    check stdin

--- src/batch_executor.rs
+++ src/batch_executor.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use std::io::{self, IsTerminal, Read};
 use std::process::ExitCode;
 
 use crate::cache::CacheCursor;
@@ -6,8 +7,15 @@ use crate::command_io::Command;
 
 pub fn run(command: Command) -> Result<ExitCode> {
     println!("running: {command:?}");
+
     let cache = CacheCursor::new(command.name.clone());
     cache.create_directory()?;
+    let mut stdin = Vec::new();
+    let mut process_stdin = io::stdin();
+    if !process_stdin.is_terminal() {
+        process_stdin.read_to_end(&mut stdin)?;
+    }
+    println!("got stdin: {stdin:?}");
 
     Ok(ExitCode::SUCCESS)
 }
