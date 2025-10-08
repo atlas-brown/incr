198656253ff1caa8a5673a7d63245884c3f69bf0
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-10-08 10:55:43 -0600

    check

--- overhead/with_try.sh
+++ overhead/with_try.sh
@@ -1,8 +1,7 @@
-rm -rf sandbox
 for i in $(seq 10)
 do
-    mkdir -p sandbox
-    ./src/try.sh -D sandbox echo "hello world!"
-    ./src/try.sh commit sandbox
-    sudo rm -rf sandbox
+    mkdir -p "sandbox_$i"
+    ./src/try.sh -D "sandbox_$i" echo "hello world!"
+    ./src/try.sh commit "sandbox_$i"
+    sudo rm -rf "sandbox_$i"
 done
