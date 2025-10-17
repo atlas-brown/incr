bb4af420a3938419a3af23a58205367e66e2ba98
Yizheng Xie <yizhengx@bu.edu>
2025-10-16 20:48:02 -0600

    Nit

--- incr.sh
+++ incr.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+
 TOP=$(git rev-parse --show-toplevel)
 tempfile=$(mktemp)
-echo "Temporary file created at $tempfile"
-echo "Processing script $1"
 python3 ${TOP}/src/scripts/insert.py --sys-path ${TOP}/target/release/incr "$1" > "$tempfile"
 bash $tempfile
