aea3329da2b235e5f2c3dadfef2b4e1167f0dffa
Evangelos Lamprou <vagos@lamprou.xyz>
2025-10-20 12:45:16 -0400

    add error message
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- incr.sh
+++ incr.sh
@@ -6,5 +6,8 @@ TOP=$(git rev-parse --show-toplevel)
 TRY_PATH="$TOP/src/scripts/try.sh"
 tempfile=$(mktemp)
 
+[ -z "$script" ] && echo "Usage: $0 <script> <cache_dir>" && exit 1
+[ -z "$cache_dir" ] && echo "Usage: $0 <script> <cache_dir>" && exit 1
+
 python3 ${TOP}/src/scripts/insert.py --sys-path ${TOP}/target/release/incr --try $TRY_PATH --cache $2 "$1" > "$tempfile"
 bash $tempfile
