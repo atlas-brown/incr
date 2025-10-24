995356382bc61c3cc6c47e5f5a6941f8c7beb2a1
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-10-24 09:50:55 -0600

    create in current

--- incr.sh
+++ incr.sh
@@ -4,10 +4,11 @@ script=$1
 cache_dir=$2
 TOP=$(git rev-parse --show-toplevel)
 TRY_PATH="$TOP/src/scripts/try.sh"
-tempfile=$(mktemp)
+tempfile=$(mktemp "$(dirname $script)/incr_script.XXXXXXXX.sh")
 
 [ -z "$script" ] && echo "Usage: $0 <script> <cache_dir>" && exit 1
 [ -z "$cache_dir" ] && echo "Usage: $0 <script> <cache_dir>" && exit 1
 
+echo "inserting: $tempfile"
 python3 ${TOP}/src/scripts/insert.py --sys-path ${TOP}/target/release/incr --try $TRY_PATH --cache $2 "$1" > "$tempfile"
 bash $tempfile
