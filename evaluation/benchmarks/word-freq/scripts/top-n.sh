c3e390ef40d3fbcf06b2c76167069146ff20a720
Evangelos Lamprou <vagos@lamprou.xyz>
2025-11-02 16:56:54 -0500

    small fix
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- evaluation/benchmarks/word-freq/scripts/top-n.sh
+++ evaluation/benchmarks/word-freq/scripts/top-n.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Top-N (1000) terms
-# from https://dl.acm.org/doi/10.1145/5948.315654
+# top-N (1000) terms
 
-cat $INPUT | tr -c 'A-Za-z' '[\n*]' | grep -v "^\s*$" | tr A-Z a-z | sort | uniq -c | sort -rn | sed 100q
+cat $INPUT | tr -c 'A-Za-z' '[\n*]' | grep -v "^\s*$" | tr A-Z a-z | sort | uniq -c | sort -rn | sed 1000q
