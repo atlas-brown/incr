7d8087ef8cc7383505c5ce4b9db3a331800aca4b
Evangelos Lamprou <vagos@lamprou.xyz>
2025-11-19 19:43:23 -0500

    update run script

--- evaluation/bash-ts/run.sh
+++ evaluation/bash-ts/run.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 cd "$(dirname "$0")" || exit 1
-cd tests
+
+[ ! -d bash ] && git clone http://git.savannah.gnu.org/bash.git
+make -C bash recho zecho printenv xcase
+
+export PATH="$PATH:$PWD/bash/tests"
 
 # First, run tests with bash
-export THIS_SH=/bin/bash
+export THIS_SH=$PWD/bash/bash
 export BASH_TSTOUT=/tmp/tstout
+
+cd tests
 
 # sh run-all > ../results.bash
 
@@ -15,4 +21,4 @@ rm -rf "$TOP/evaluation/bash-ts/cache"
 export THIS_SH=$TOP/evaluation/bash-ts/incr.sh
 export INCR_TSTOUT=/tmp/tstout
 
-sh run-all > ../results.incr
+sh run-varenv > ../results.incr
