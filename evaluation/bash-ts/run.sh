7620a452ced51c4c63a551912d4ae47acc360250
Evangelos Lamprou <vagos@lamprou.xyz>
2025-11-22 06:34:19 +0200

    update test run script

--- evaluation/bash-ts/run.sh
+++ evaluation/bash-ts/run.sh
@@ -2,20 +2,24 @@
 
 cd "$(dirname "$0")" || exit 1
 
-[ ! -d bash ] && git clone http://git.savannah.gnu.org/bash.git
+# [ ! -d bash ] && git clone https://github.com/bminor/bash && git -C bash checkout c5c97b3
+# ( cd bash; CC=cc ./configure)
 # make -C bash -j4
 # make -C bash recho zecho printenv xcase -j4
 
-export PATH="$PATH:$PWD/bash/tests"
+target_test="run-precedence"
+
+export PATH="$PATH:$PWD/bash"
 export TMPDIR=/tmp
 
 # First, run tests with bash
 export THIS_SH=$PWD/bash/bash
 export BASH_TSTOUT=/tmp/tstout
 
-cd tests
+# cd tests
+cd tests-normln
 
-# sh run-varenv > ../results.bash
+sh $target_test > ../results.bash
 
 # Then, run tests with incr
 TOP=$(git rev-parse --show-toplevel)
@@ -23,4 +27,6 @@ rm -rf "$TOP/evaluation/bash-ts/cache"
 export THIS_SH=$TOP/evaluation/bash-ts/incr.sh
 export INCR_TSTOUT=/tmp/tstout
 
-sh run-all > ../results.incr
+sh $target_test > ../results.incr
+
+diff ../results.bash ../results.incr > ../results.diff
