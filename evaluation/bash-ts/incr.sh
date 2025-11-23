bad3143c029b4e040801db5797243191c8e3738a
Evangelos Lamprou <vagos@lamprou.xyz>
2025-11-23 05:06:24 +0200

    fix quoting

--- evaluation/bash-ts/incr.sh
+++ evaluation/bash-ts/incr.sh
@@ -4,4 +4,4 @@ __TOP=$(git rev-parse --show-toplevel)
 export INCR_SHELL="$__TOP/evaluation/bash-ts/bash/bash"
 cache="/tmp/cache"
 mkdir -p "$cache"
-$__TOP/incr.sh $@ "$cache" 
+$__TOP/incr.sh "$@" "$cache" 
