543eb2ef10eb69c5fba26b0ec362157c8fe56fd4
Evangelos Lamprou <vagos@lamprou.xyz>
2025-11-23 04:18:11 +0200

    change cache

--- evaluation/bash-ts/incr.sh
+++ evaluation/bash-ts/incr.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 __TOP=$(git rev-parse --show-toplevel)
-export INCR_SHELL=$__TOP/evaluation/bash-ts/bash/bash
-$__TOP/incr.sh "$@" "$__TOP/evaluation/bash-ts/cache"
+export INCR_SHELL="$__TOP/evaluation/bash-ts/bash/bash"
+cache="/tmp/cache"
+mkdir -p "$cache"
+$__TOP/incr.sh $@ "$cache" 
