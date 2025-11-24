c4302908213ae0dd4415ed68f2d5c4cff1efeffd
Evangelos Lamprou <vagos@lamprou.xyz>
2025-11-24 22:49:52 +0200

    fix run script

--- evaluation/bash-ts/run.sh
+++ evaluation/bash-ts/run.sh
@@ -8,39 +8,42 @@ cd "$(dirname "$0")" || exit 1
 # make -C bash recho zecho printenv xcase -j4
 
 sudo rm -rf /tmp/cache
-rm -f out.* results.*
+rm -f results.*
 
-[ -z "$1" ] && exit 1
-
-results_dir="$PWD/results"
-test="$1"
-target_test="run-$test"
-[ -z "$target_test" ] && exit 1
+results_dir="$PWD/results-redo"
+mkdir -p "$results_dir"
 top=$(git rev-parse --show-toplevel)
 export PATH="$PATH:$PWD/bash"
 export TMPDIR=/tmp
 export BASH_TSTOUT=/tmp/tstout
 export BUILD_DIR="$PWD/bash"
 
-mkdir -p "$results_dir"
-
 cd tests || exit 1
 
-# First, run tests with bash
-export THIS_SH=$top/evaluation/bash-ts/bash/bash
-$THIS_SH $target_test > ../results.bash
-cp ../results.bash "$results_dir"/$test.results.bash
-
-# Then, run tests with incr
-export THIS_SH=$top/evaluation/bash-ts/incr.sh
-$THIS_SH $target_test > ../results.incr
-cp ../results.incr "$results_dir"/$test.results.incr
-
-# Finally, diff results
-diff ../results.bash ../results.incr > ../results.diff
-nl()
-{
-	# Normalize line numbers
-	sed -E 's/line [0-9]+/line xxxx/' $1 
+run_test() {
+  test="$1"
+  target_test="run-$test"
+  [ -z "$target_test" ] && exit 1
+  
+  
+  
+  # First, run tests with bash
+  export THIS_SH=$top/evaluation/bash-ts/bash/bash
+  $THIS_SH $target_test > ../results.bash
+  cp ../results.bash "$results_dir"/$test.results.bash
+  
+  # Then, run tests with incr
+  export THIS_SH=$top/evaluation/bash-ts/incr.sh
+  $THIS_SH $target_test > ../results.incr
+  cp ../results.incr "$results_dir"/$test.results.incr
 }
-# diff <(nl ../out.bash) <(nl ../out.incr) > ../out.diff
+
+if [ -n "$1" ]; then
+	run_test "$1"
+	exit 0
+fi
+
+tests="$(find . -type f -name "run-*" | sed 's/run-//' | grep -v all | grep -v minimal)"
+for test in $tests; do
+	run_test "${test#./}"
+done
