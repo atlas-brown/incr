f0d59d27056ffbba00787ee1a5db22b7bc468151
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-11-24 11:04:39 -0700

    chunk test

--- evaluation/microbenchmarks/chunk/execute.sh
+++ evaluation/microbenchmarks/chunk/execute.sh
@@ -6,3 +6,14 @@ export IN="$TOP/evaluation/microbenchmarks/chunk/inputs/pg-min"
 sleep 0.01
 time ./scripts/chunk.sh > output.txt
 sha256sum output.txt
+
+rm -- "$IN"/*_dup.*
+file=$(find "$IN" -maxdepth 1 -type f -printf '%f\n' | sort | head -n1)
+duplicated="$IN/${file%.*}_dup.${file##*.}"
+cp "$IN/$file" "$duplicated"
+
+sleep 0.01
+time ./scripts/chunk.sh > output.txt
+sha256sum output.txt
+
+rm -f "$duplicated"
