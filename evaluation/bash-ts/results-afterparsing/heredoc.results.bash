68802a7c4d5ec3d3ccbb642feb6e90d5953bd988
Evangelos Lamprou <vagos@lamprou.xyz>
2025-11-25 05:29:42 +0200

    update results after parsing

--- evaluation/bash-ts/results-afterparsing/heredoc.results.bash
+++ evaluation/bash-ts/results-afterparsing/heredoc.results.bash
@@ -1,8 +1,3 @@
-122a123,129
-> ./heredoc7.sub: line 17: warning: command substitution: 1 unterminated here-document
-> foo bar
-> ./heredoc7.sub: line 21: after: command not found
-> ./heredoc7.sub: line 29: warning: here-document at line 29 delimited by end-of-file (wanted `EOF')
-> ./heredoc7.sub: line 29: foobar: command not found
-> ./heredoc7.sub: line 30: EOF: command not found
-> grep: *.c: No such file or directory
+92a93,94
+> ./heredoc3.sub: line 98: warning: here-document at line 96 delimited by end-of-file (wanted `EOF')
+> ./heredoc3.sub: line 99: syntax error: unexpected end of file
--- evaluation/bash-ts/results-afterparsing/invocation.results.bash
+++
@@ -1,4 +0,0 @@
-103c103
-< ./invocation.tests: line 71: ./x23: cannot execute: required file not found
----
-> ./invocation.tests: ./x23: nosuchfile: bad interpreter: No such file or directory
