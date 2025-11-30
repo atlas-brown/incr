2b128f3b37887b7c4075c9be38d6a7a4876e0b01
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-11-29 17:43:27 -0700

    batch result

--- src/main.rs
+++ src/main.rs
@@ -121,7 +121,7 @@ fn parse_input() -> Result<Option<Input>> {
 
         batch_executor: arguments.batch_executor,
         short_circuit: arguments.short_circuit,
-        compress_output: true,
+        compress_output: arguments.compress_output,
         full_tracing: arguments.full_tracing,
         enable_annotations: arguments.enable_annotations,
         skip_introspection: arguments.skip_introspection,
