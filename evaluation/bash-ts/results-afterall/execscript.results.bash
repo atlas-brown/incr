afc4ac964a7565c752c073dcd631a0e15309e5ca
Evangelos Lamprou <vagos@lamprou.xyz>
2025-12-02 11:04:44 -0500

    update results
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- evaluation/bash-ts/results-afterall/execscript.results.bash
+++ evaluation/bash-ts/results-afterall/execscript.results.bash
@@ -1,28 +1,30 @@
-10,15c10,11
-< Traceback (most recent call last):
-<   File "/home/vagozino/incr/src/scripts/insert.py", line 3, in <module>
-<     import libbash
-< ModuleNotFoundError: No module named 'libbash'
+10,12c10,11
+< bash: notthere: No such file or directory
 < cp: cannot stat 'notthere': No such file or directory
 < 0
 ---
 > /tmp/bash: notthere: No such file or directory
 > 127
-28d23
+25d23
 < trap -- 'echo USR1' SIGUSR1
-29a25
+26a25
 > trap -- 'echo USR1' SIGUSR1
-34d29
+31d29
 < trap -- 'echo USR1' SIGUSR1
-35a31
+32a31
 > trap -- 'echo USR1' SIGUSR1
-38,41c34,37
-< ./execscript: line 71: notthere: Permission denied
-< 126
-< ./execscript: line 74: notthere: Permission denied
-< 126
+35c34
+< ./execscript: line 71: notthere: command not found
 ---
 > ./execscript: line 71: notthere: No such file or directory
-> 127
+37c36
+< ./execscript: line 74: notthere: command not found
+---
 > ./execscript: line 74: notthere: No such file or directory
-> 127
+43c42
+< /home/vagozino/incr/venv/bin:/home/vagozino/.cargo/bin:/opt/orbstack-guest/bin-hiprio:/opt/orbstack-guest/data/bin/cmdlinks:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/opt/orbstack-guest/bin:/home/vagozino/incr/evaluation/bash-ts/bash
+---
+> unset
+63,64d61
+< mkdir: cannot create directory 'testa': File exists
+< mkdir: cannot create directory 'testb': File exists
