7245ede18e9c915bd5579ec349e9c85998b051c0
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-12-03 21:53:21 -0700

    setup

--- evaluation/benchmarks/dpt/execute_manual.sh
+++ evaluation/benchmarks/dpt/execute_manual.sh
@@ -1,3 +1,3 @@
 ./execute.sh
-cp ./outputs/timing.csv ../../default_results/default_1/dpt-time.csv
-du -sb ./cache > ../../default_results/default_1/dpt-size.txt
+cp ./outputs/timing.csv ../../default_results/default_3/dpt-time.csv
+du -sb ./cache > ../../default_results/default_3/dpt-size.txt
