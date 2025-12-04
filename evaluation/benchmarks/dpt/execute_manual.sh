f8bc21b40bd0ba26adbafaa0b29cda00c6c55357
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-12-04 08:26:18 -0700

    annotation

--- evaluation/benchmarks/dpt/execute_manual.sh
+++ evaluation/benchmarks/dpt/execute_manual.sh
@@ -1,3 +1,3 @@
 ./execute.sh
-cp ./outputs/timing.csv ../../default_results/default_3/dpt-time.csv
-du -sb ./cache > ../../default_results/default_3/dpt-size.txt
+cp ./outputs/timing.csv ../../annotation_results/annotation_1/dpt-time.csv
+du -sb ./cache > ../../annotation_results/annotation_1/dpt-size.txt
